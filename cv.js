0c547e0c8285216c69159ae05bcf6838aca9faad
Franco Dellarupe <124899048+Francdell@users.noreply.github.com>
2023-02-25 12:18:12 -0300

    Initial commit

---
+++ cv.js
@@ -0,0 +1,7 @@
+document.getElementById("boton").addEventListener('click', miFuncion)
+
+function miFuncion() {
+  document.getElementById("c-1").innerHTML = "Telefono: 667-8784";
+  document.getElementById("c-2").innerHTML = "Mail: kelly.moore@example.com";
+  document.getElementById("c-3").innerHTML = "Direccion: 7417 Northaven Rd";
+}
